3bac004b4a7bae321e96c5c86cb0bf615ec29663
Peter Bishop <pjb.den@gmail.com>
2021-06-01 13:59:29 -0600

    completed get all route, added helper for date format, added homepage view file and ref in main

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -12,18 +12,15 @@ router.get("/", (req, res) => {
         attributes: ["id", "comment_text", "post_id", "created_at"],
       },
     ],
-  }).then((postData) => {
-    res.json(postData);
-  });
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("homepage", { posts });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
-// .then((dbPostData) => {
-//   const posts = dbPostData.map((post) => post.get({ plain: true }));
-//   res.render("homepage", { posts, loggedIn: req.session.loggedIn });
-// })
-// .catch((err) => {
-//   console.log(err);
-//   res.status(500).json(err);
-// });
-// });
 
 module.exports = router;
