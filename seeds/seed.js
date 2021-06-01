0e2b0f9c4372bf1d14122d748180108084e1c393
kevinjwesley <kevin.john.wesley@gmail.com>
2021-06-01 12:15:52 -0600

    created postData to seed

---
+++ seeds/seed.js
@@ -0,0 +1,23 @@
+const sequelize = require("../config/connection");
+const { Comment, Post } = require("../models");
+
+const postData = require("./post_data.json");
+const commentData = require("./comment_data.json");
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  await Post.bulkCreate(postData, {
+    individualiHooks: true,
+    returning: true,
+  });
+
+  await Comment.bulkCreate(commentData, {
+    individualiHooks: true,
+    returning: true,
+  });
+
+  process.exit(0);
+};
+
+seedDatabase();
