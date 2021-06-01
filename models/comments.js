ebb59c37436d10a81e7871cd1cc466b141fa22c4
Peter Bishop <pjb.den@gmail.com>
2021-06-01 12:01:41 -0600

    created models for Posts and Comments

---
+++ models/comments.js
@@ -0,0 +1,37 @@
+//Model for comments to be added to the Posts
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    comment_text: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [2],
+      },
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "post",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize: sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "comment",
+  }
+);
+
+module.exports = Comment;
