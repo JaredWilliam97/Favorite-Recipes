1b3f4b896395e8532ca4fa0f54c930f07707b403
kevinjwesley <kevin.john.wesley@gmail.com>
2021-06-02 14:10:35 -0600

    created newComment.js, commentRoute.js api and started working on comment feature

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,4 +1,17 @@
-const router = require('express').Router();
-const { comments } = require('../../models');
+const router = require("express").Router();
+const { Comment } = require("../../models");
+
+// CREATE
+router.post("/", (req, res) => {
+  Comment.create({
+    comment: req.body.comment_text,
+    post_id: req.body.post_id,
+  })
+    .then((data) => res.json(data))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
