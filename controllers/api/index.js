461a330f8126fae341743aac1a72bdff9fc71ab6
Peter Bishop <pjb.den@gmail.com>
2021-06-02 10:24:45 -0600

    added post route to POSTS

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,3 @@
-const router = require('express').Router();
-const postRoutes = require('./postRoutes');
-
-router.use('/posts', postRoutes);
-
 const router = require("express").Router();
 const postRoutes = require("./postRoutes");
 const commentRoutes = require("./commentRoutes");
--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,19 +1,29 @@
 const router = require("express").Router();
 const { Post } = require("../../models");
 
+// router.post('/', async (req, res) => {
+//     try {
+//       const newPost = await Post.create({
 
-router.post('/', async (req, res) => {
-    try {
-      const newPost = await Post.create({
-        
-      title: req.body.title,
-      content: req.body.content,
+//       title: req.body.title,
+//       content: req.body.content,
+//     });
+
+//   res.status(200).json(newPost);}
+//   catch (err){ res.status(400).json(err);}
+
+//     })
+
+router.post("/", (req, res) => {
+  Post.create({
+    title: req.body.title,
+    content: req.body.content,
+  })
+    .then((data) => res.json(data))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
-      
-  res.status(200).json(newPost);}
-  catch (err){ res.status(400).json(err);}
-      
+});
 
-    
-    })
 module.exports = router;
