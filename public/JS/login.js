9d7bbc7b2eebda8b50b047204a7f0a7a3b907f8d
Lisa Barton <lisabarton23@yahoo.com>
2021-06-03 11:23:34 -0600

    added fun library to login page

--- public/JS/login.js
+++ public/JS/login.js
@@ -15,7 +15,7 @@ const loginFormHandler = async (event) => {
     
     })
     if (response.ok) {
-    document.location.replace('/post'); 
+    document.location.replace('api/post'); 
     
     }
     else{
