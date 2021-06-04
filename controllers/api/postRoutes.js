bc4778fab2c6ddefae58c9d21025a799f93f2bbd
kevinjwesley <kevin.john.wesley@gmail.com>
2021-06-04 14:59:52 -0600

    woking on username function in postRoutes.js and newPost.js

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,6 +1,6 @@
 const sequelize = require("../../config/connection");
 const router = require("express").Router();
-const { Post } = require("../../models");
+const { Post, User } = require("../../models");
 const withAuth = require("../../utils/auth");
 //RETRIEVE
 router.get("/newpost", (req, res) => {
@@ -41,18 +41,35 @@ router.get("/post/:id", (req, res) => {
     });
 });
 
+//
+
 //CREATE
 router.post("/", withAuth, (req, res) => {
-  Post.create({
-    title: req.body.title,
-    content: req.body.content,
-    name: req.body.name,
+  // QUERY USER TABLE
+  // PULL NAME OUT OF OBJ
+  User.findOne({
+    where: {
+      id: req.session.user_id,
+    },
   })
-    .then((data) => res.json(data))
+    .then((userData) => {
+      Post.create({
+        title: req.body.title,
+        content: req.body.content,
+        name: userData.name,
+      })
+        .then((data) => res.json(data))
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+      console.log(userData.name);
+    })
     .catch((err) => {
       console.log(err);
-      res.status(500).json(err);
     });
+
+  res.send("ok");
 });
 
 //UPDATE
--- public/JS/newPost.js
+++ public/JS/newPost.js
@@ -3,20 +3,14 @@ async function newFormHandler(event) {
 
   const title = document.querySelector("#postTitle").value;
   const content = document.querySelector("#content").value;
-  const userName = fetch(`/api/users/${name}`)
-    .then((data) => {
-      return data.json();
-    })
-    .then((post) => {
-      console.log(post.name);
-    });
+  // const userName = fetch(`/api/users/${name}`)
 
   const response = await fetch(`/api/posts`, {
     method: "POST",
     body: JSON.stringify({
       title,
       content,
-      userName,
+      name,
     }),
     headers: {
       "Content-Type": "application/json",
